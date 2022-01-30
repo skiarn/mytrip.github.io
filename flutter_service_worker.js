e60347a5ec5d6c9d4cfe6b40ef4230af63f4e9cd
skiarn <andreaswestberg91@gmail.com>
2022-01-30 20:40:33 +0100

    improved app

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "9daf69d55a41a67384403e0c8f3d3989",
-"index.html": "c97e143167bc5d62c8eb1494d2a70860",
-"/": "c97e143167bc5d62c8eb1494d2a70860",
-"main.dart.js": "0fe90e52a05161e55b4560ce3e82e9d0",
+"index.html": "052aa16aa08db3037364db9d109b4b87",
+"/": "052aa16aa08db3037364db9d109b4b87",
+"main.dart.js": "4407f8ae318495a6ce4dd40349f918ff",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
